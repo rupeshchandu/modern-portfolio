48e17794f58f0d01b8456686aff8c542627db8e0
Rupesh Chandu <lvrupeshch@gmail.com>
2023-06-28 19:05:39 +0530

    Modified

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,26 +34,3 @@ export default function App({ Component, pageProps }) {
   )
 }
 
-
-
-// export default function App({ Component, pageProps }) {
-//   return (
-//     <>
-//       <Head>
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <link rel="icon" href="https://soumyaswaroopsootar.vercel.app/SSS-LOGO.png" type="image/x-icon" />
-//       </Head>
-//       <main
-//         className={`${montserrat.variable} font-mont bg-light w-full dark:bg-dark min-h-screen`}
-//       >
-//         {/* <div className="text-xs absolute top-0 bg-red-700 text-lime-100">
-//           THIS PAGE IS BEING BUILDING NOW...GOOD THINGS TAKE TIME.....80% Built⚠️
-//         </div> */}
-//         <Navbar />
-//         <Component {...pageProps} />
-//         <Footer/>
-//       </main>
-//     </>
-//   );
-// }
-
