f9013674d23015aa85ca7857c4c7ac8f999458dc
Rupesh chandu <lvrupeshch@gmail.com>
2023-08-04 13:40:03 +0530

    modified

--- src/Components/HireMe.js
+++ src/Components/HireMe.js
@@ -4,7 +4,7 @@ import React from "react";
 const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-9 flex items-center 
-    justify-center md:absolute md:right-9 md:top-4 md:left-auto md:bottom-auto sm:right-4'>
+    justify-center md:left-auto md:bottom-[930px] md:right-9 md:top-11 sm:right-4 md:absolute z-20'>
           <Link href= "mailto:lvrupeshch@gmail.com" className='flex items-center justify-center
                 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full 
               font-semibold hover:bg-light hover:text-dark dark:text-dark dark:bg-light
