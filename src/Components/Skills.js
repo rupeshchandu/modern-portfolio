6e4f8a4f1c021c1124f974442b2a74e1e4af6b29
Rupesh chandu <lvrupeshch@gmail.com>
2023-08-04 14:09:22 +0530

    modified

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -25,7 +25,7 @@ const Skill = ({name,x,y})=>{
 const Skills = () => {
   return (
     <>
-        <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32 md:mb-16 sm:text-4xl'>
+        <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32 md:mb-4'>
             Skills
         </h2>
         <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark 
